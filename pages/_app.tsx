c894359ae8c08a6d7b9bbcb21c52434d324b1a1a
Pierce <P.Hahn94@outlook.com>
2022-10-05 13:28:27 -0300

    removed next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,8 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Session } from 'next-auth';
-import { SessionProvider } from 'next-auth/react';
 
-function MyApp({
-  Component,
-  pageProps,
-}: AppProps<{
-  session: Session;
-}>) {
-  return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
