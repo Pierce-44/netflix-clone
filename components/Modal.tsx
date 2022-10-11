0dfd870b7ac04d171b508d9080bcba63a5a7c79f
Pierce-44 <piercehahn.coding@gmail.com>
2022-10-11 13:55:11 +0100

    SVG prop changes

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -38,12 +38,12 @@ export default function Modal({
         <svg
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M6 18L18 6M6 6l12 12"
           />
         </svg>
